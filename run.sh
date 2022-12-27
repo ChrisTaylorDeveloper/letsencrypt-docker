2e7d557fe5d6e7f66611724cc9406bfb2ebed794
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-27 20:00:14 +0000

    temp

--- run.sh
+++ run.sh
@@ -11,3 +11,5 @@ fi
 docker-compose up --build -d nginx
 
 # Pause here until http://worldpeace.cloud response code is 200
+curl --output /dev/null --connect-timeout 5 --max-time 20 \
+    --write-out '%{http_code}' -s -S https://karltayloreducation.com
