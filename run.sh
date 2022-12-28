b500edd88660d0ec7651b79acd64800f3b175006
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-28 22:02:24 +0000

    temp

--- run.sh
+++ run.sh
@@ -28,6 +28,4 @@ until [[ $(domain_response_code) -eq 200 ]]; do
     sleep 2
 done
 
-docker-compose up --build -d certbot
-
-docker-compose --exit-code-from certbot
+docker-compose up --build -d --exit-code-from certbot
