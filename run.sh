35b4842f6a38b4df4ee27dee659bc5ab17655262
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-01-02 18:17:16 +0000

    temp

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ docker-compose down
 
 # Clean up after previous runs 
 rm -rf ${VOLUME_CERTBOT_ETC}
-mkdir -${VOLUME_CERTBOT_ETC}
+mkdir ${VOLUME_CERTBOT_ETC}
 
 # Setting these variables with prune Docker and delete all Volumes. 
 if [[ $CERTBOT_DOCKER_PRUNE == "1" ]]
