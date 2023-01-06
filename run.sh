a178e4423ff1f4d35d618927694ea923a83a1d3e
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-01-06 16:41:52 +0100

    temp

--- run.sh
+++ run.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -xv
 
 function domain_response_code () {
-    echo "$(curl --output /dev/null --connect-timeout 5 --max-time 10 \
-        --write-out '%{http_code}' -s -S http://worldpeace.cloud)"
+    curl --output /dev/null --connect-timeout 5 --max-time 10 \
+        --write-out '%{http_code}' -s -S http://worldpeace.cloud
 }
 
 function nginx_up () {
     docker run --name nginx -d \
-        -p "80:80" \
-        -p "443:443" \
-        -v certbot_etc:/etc/letsencrypt \
-        -v certbot_var:/var/lib/letsencrypt \
-        -v html:/usr/share/nginx/html \
-        -v /home/dock/letsencrypt-docker/nginx_conf:/etc/nginx/conf.d \
-        nginx:1.23.3
+    -p "80:80" \
+    -p "443:443" \
+    -v certbot_etc:/etc/letsencrypt \
+    -v certbot_var:/var/lib/letsencrypt \
+    -v html:/usr/share/nginx/html \
+    -v /home/dock/letsencrypt-docker/nginx_conf:/etc/nginx/conf.d \
+    nginx:1.23.3
+}
+
+function certbot_up () {
+    docker run --name certbot -d \
+    -v certbot_etc:/etc/letsencrypt \
+    -v certbot_var:/var/lib/letsencrypt \
+    -v html:/var/www/html \
+    -v dhparam:/etc/ssl/certs \
+    certbot/certbot \
+    certonly --webroot --webroot-path=/var/www/html --email chris@christaylordeveloper.co.uk --agree-tos --no-eff-email --force-renewal -d worldpeace.cloud --staging --break-my-certs
 }
 
 # Cleanup Docker.
@@ -54,7 +64,7 @@ docker volume create \
     dhparam 
 
 # Run nginx for the first time.
-nginx_cont=$(nginx_up)
+nginx_cont=$(nginx_up())
 nginx_status=$(docker inspect "${nginx_cont}" --format='{{.State.ExitCode}}')
 if [[ "${nginx_status}" -ne 0 ]];
 then
@@ -68,13 +78,7 @@ until [[ $(domain_response_code) -eq 200 ]]; do
 done
 
 # Run certbot service for the first time.
-certbot_cont=$(docker run --name certbot -d \
-    -v certbot_etc:/etc/letsencrypt \
-    -v certbot_var:/var/lib/letsencrypt \
-    -v html:/var/www/html \
-    -v dhparam:/etc/ssl/certs \
-    certbot/certbot \
-    certonly --webroot --webroot-path=/var/www/html --email chris@christaylordeveloper.co.uk --agree-tos --no-eff-email --force-renewal -d worldpeace.cloud --staging --break-my-certs)
+certbot_cont=$(certbot_up())
 certbot_status=$(docker inspect "${certbot_cont}" --format='{{.State.ExitCode}}')
 if [[ "${certbot_status}" -ne 0 ]];
 then
