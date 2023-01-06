f02731a7f406371e3d263d0ec1ad1482855ea10a
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-01-06 14:43:54 +0100

    temp

--- run.sh
+++ run.sh
@@ -5,46 +5,55 @@ function domain_response_code () {
         --write-out '%{http_code}' -s -S http://worldpeace.cloud)"
 }
 
-# Start with all Docker containers shutdown.
+# Start with Containers shutdown.
 docker-compose down
 
-# Clean up after previous runs 
+# Cleanup git.
 echo $1 | sudo -S git clean -fd
 git checkout -- .
-mkdir ${VOLUME_CERTBOT_ETC}
 
-# Setting these variables with prune Docker and delete all Volumes. 
-if [[ $CERTBOT_DOCKER_PRUNE == "1" ]]
-then
-    docker system prune -a --force
-fi
-if [[ $CERTBOT_DOCKER_VOLS_REMOVE == "1" ]]
-then
-    docker volume rm $(docker volume ls -q)
-fi
+# Clean up dirs.
+#mkdir ${VOLUME_CERTBOT_ETC}
+
+# Clean up docker.
+docker system prune -a --force
+docker volume rm $(docker volume ls -q)
+
+# Create Docker Volumes.
+docker volume create certbot_var
+# docker volume create \
+#     --driver local \
+#     -o type=none \
+#     -o o=bind \
+#     -o device=bind \
+#   sshvolume
+
 
 # Run nginx for the first time and exit if there was a problem.
-if ! $(docker-compose up --build -d nginx);
-then
-    echo "nginx service failed"
-    exit 1
-fi
+# if ! $(docker-compose up --build -d nginx);
+# then
+#     echo "nginx service failed"
+#     exit 1
+# fi
 
 # Pause here until http://worldpeace.cloud responses with 200.
-until [[ $(domain_response_code) -eq 200 ]]; do
-    sleep 2
-done
+# until [[ $(domain_response_code) -eq 200 ]]; do
+#     sleep 2
+# done
 
 # Run certbot and exit if there was a problem.
-if ! $(docker-compose up --build -d certbot);
-then
-    echo "certbot service failed"
-    exit 1
-fi
+# if ! $(docker-compose up --build -d certbot);
+# then
+#     echo "certbot service failed"
+#     exit 1
+# fi
+
+# Should probably stop nginx here
 
 # Swap over the basic nginx conf for the https conf.
-rm ./nginx_conf/nginx.conf
-cp ./nginx-https.conf ./nginx_conf/nginx.conf
+# rm ./nginx_conf/nginx.conf
+# cp ./nginx-https.conf ./nginx_conf/nginx.conf
 
+# Should probably start nginx here!!!
 # Restart nginx using the https conf. 
 # docker-compose restart nginx
